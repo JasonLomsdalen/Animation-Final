92834de6f5b9f6e32b38ecbd16fb67cd817b0a06
JasonLomsdalen <jlomsdal@calpoly.edu>
2018-05-21 16:00:36 -0700

    Changed up colors.

--- src/main.cpp
+++ src/main.cpp
@@ -177,7 +177,7 @@ public:
         // Init
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glClearColor(0.3f, 0.7f, 0.8f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         
